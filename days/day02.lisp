60f78636486847406ad9ac918bbe9d1207ff847b
DavidBarringer <davidlbarringer@aol.com>
2022-12-02 18:36:37 +0000

    Day 2 w/ comments

--- days/day02.lisp
+++ days/day02.lisp
@@ -1,13 +1,25 @@
 ;; Put the given solutions for the examples here
-(setf test-sol-a "No test solution")
-(setf test-sol-b "No test solution")
+(setf test-sol-a 15)
+(setf test-sol-b 12)
 
-;; Turn the input file into whatever form you will use for both parts
-;; (get-file-lines) and (get-file-string) will be useful
-(defun parse-input (input-file))
+(defun parse-input (input-file)
+  "Remove the spaces from each line, then turn into a variable name
+   this is a surprise tool to help us later"
+  (mapcar (lambda (s) (intern (remove #\  s))) (get-file-lines input-file)))
 
-;; Returns the solution for part a
-(defun part-a (parsed-input))
+(defmacro with-scores (scores body)
+  "Makes a scope with each result assigned to its score, 
+   declare special allows these to be accessed with symbol-value
+   then evaluate the body"
+  `(let ,scores (declare ,(cons 'special (mapcar 'CAR scores))) ,body))
+
+;; Returns the soluton for part a
+(defun part-a (parsed-input)
+  (with-scores ((AX 4) (AY 8) (AZ 3) (BX 1) (BY 5) (BZ 9) (CX 7) (CY 2) (CZ 6))
+    (apply '+ (mapcar 'symbol-value parsed-input))))
+
 
 ;; Returns the solution for part b
-(defun part-b (parsed-input))
+(defun part-b (parsed-input)
+  (with-scores ((AX 3) (AY 4) (AZ 8) (BX 1) (BY 5) (BZ 9) (CX 2) (CY 6) (CZ 7))
+    (apply '+ (mapcar 'symbol-value parsed-input))))
