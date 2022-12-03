9ab04555c835f9d772ef99471a7b9f5e19150b58
DavidBarringer <davidlbarringer@aol.com>
2022-12-03 18:43:44 +0000

    Day 3 comments

--- days/day03.lisp
+++ days/day03.lisp
@@ -2,26 +2,27 @@
 (setf test-sol-a 157)
 (setf test-sol-b 70)
 
-;; Turn the input file into whatever form you will use for both parts
-;; (get-file-lines) and (get-file-string) will be useful
 (defun parse-input (input-file)
+  "Turns each line into a list of characters"
   (mapcar (lambda (s) (concatenate 'list s)) (get-file-lines input-file)))
 
 (defun score-char (val)
   (if (char< val #\a) (+ 26 (- (char-code val) 64)) (- (char-code val) 96)))
 
 (defun intersect (l1 &rest lists)
+  "Function to calculate the intersection of
+   multiple lists"
   (if (null (CAR lists)) l1 (intersect (intersection l1 (CAR lists)) (CADR lists))))
 
 (defmacro with-grouping-sum (grouping body)
+  "Takes a list of 'groups' and applies the body to each
+   group, then sums the result of each"
   `(loop for group in ,grouping sum ,body))
 
-;; Returns the solution for part a
 (defun part-a (parsed-input)
   (with-grouping-sum (mapcar (lambda (l) (list (subseq l 0 (/ (length l) 2)) (nthcdr (/ (length l) 2) l))) parsed-input)
     (apply '+ (mapcar 'score-char (remove-duplicates (apply 'intersect group))))))
 
-;; Returns the solution for part b
 (defun part-b (parsed-input) 
   (with-grouping-sum (loop for (x y z) on parsed-input by #'CDDDR collect (list x y z))
     (apply '+ (mapcar 'score-char (remove-duplicates (apply 'intersect group))))))
