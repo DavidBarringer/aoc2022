213798a3e464fc89bd1ea2ed434fe5031840b8d9
DavidBarringer <davidlbarringer@aol.com>
2022-12-04 12:22:13 +0000

    Day 4

--- days/day04.lisp
+++ days/day04.lisp
@@ -1,13 +1,22 @@
 ;; Put the given solutions for the examples here
-(setf test-sol-a "No test solution")
-(setf test-sol-b "No test solution")
+(setf test-sol-a 2)
+(setf test-sol-b 4)
 
 ;; Turn the input file into whatever form you will use for both parts
 ;; (get-file-lines) and (get-file-string) will be useful
-(defun parse-input (input-file))
+(defun parse-input (input-file)
+  (mapcar (lambda (s) (mapcar (lambda (s2) (mapcar 'parse-integer (split "-" s2))) (split "," s))) (get-file-lines input-file)))
 
 ;; Returns the solution for part a
-(defun part-a (parsed-input))
+(defun part-a (parsed-input)
+  (loop for ((v w) (x y)) in parsed-input
+	count (OR (AND (<= v x) (>= w y))
+		  (AND (<= x v) (>= y w)))))
+
 
 ;; Returns the solution for part b
-(defun part-b (parsed-input))
+(defun part-b (parsed-input)
+  (loop for ((v w) (x y)) in parsed-input
+	count (OR (AND (<= v x) (>= w x))
+		  (AND (<= x v) (>= y v)))))
+
